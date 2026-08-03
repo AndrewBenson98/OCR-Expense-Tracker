f719983560ca315642b78a21e770bd6766fbceee
Andrew Benson <andrewbenson123@gmail.com>
2026-08-03 18:12:29 -0400

    initial commit

---
+++ src/main/java/com/benson/ocr_expense_tracker/OcrExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.benson.ocr_expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benson/ocr_expense_tracker/OcrExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benson.ocr_expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
